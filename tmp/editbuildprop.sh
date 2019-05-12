b4295c261df93ca25eed92840a715c4877271745
Anonymous <none@none.none>
2019-05-12 13:24:23 +0200

    Initial commit

---
+++ tmp/editbuildprop.sh
@@ -0,0 +1,40 @@
+#!/sbin/sh
+
+sbp="/system/build.prop"
+vbp="/system/vendor/build.prop"
+
+# If system build.prop backup exists, restore it
+if [ -f /system/build.prop.bak ];
+then
+    rm -rf $sbp
+    cp $sbp.bak $sbp
+else
+    cp $sbp $sbp.bak
+fi
+# If vendor build.prop backup exists, restore it
+if [ -f /system/vendor/build.prop.bak ];
+then
+    rm -rf $vbp
+    cp $vbp.bak $vbp
+else
+    cp $vbp $vbp.bak
+fi
+
+echo " " >> $bp
+cat > /tmp/build.prop <<DELIM
+persist.vendor.radio.multisim.config=dsds
+ro.telephony.default_network=9,1
+DELIM
+
+for prop in `cat /tmp/build.prop`;do
+  export newprop=$(echo ${prop} | cut -d '=' -f1)
+
+  sed -i "/${newprop}/d" /system/build.prop
+  echo $prop >> /system/build.prop
+done
+
+sed -i 's/f8331/f8332/g' /system/build.prop
+sed -i 's/F8331/F8332/g' /system/build.prop
+
+sed -i 's/f8331/f8332/g' /system/vendor/build.prop
+sed -i 's/F8331/F8332/g' /system/vendor/build.prop
