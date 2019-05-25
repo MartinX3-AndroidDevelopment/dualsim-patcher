615a3cd05e14035bacd813974471e56a0b95fe1d
Anonymous <none@none.none>
2019-05-25 18:39:04 +0200

    Explicitly include files, drop unused cert stuff

--- create_zip.sh
+++ create_zip.sh
@@ -1,2 +1,4 @@
+#!/usr/bin/env sh
 mkdir -p zips
-zip zips/sony-dualsim-patcher.zip tmp/* META-INF/**/*
+rm zips/sony-dualsim-patcher.zip
+zip zips/sony-dualsim-patcher.zip tmp/* META-INF/com/google/android/update-binary META-INF/com/google/android/updater-script
