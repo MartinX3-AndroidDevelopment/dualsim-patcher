87f3220044d340af2502d5b13e7aaeb501e076e4
Anonymous <none@none.none>
2019-05-30 17:00:09 +0200

    Bump version

--- create_zip.sh
+++ create_zip.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-VERSION=2
+VERSION=3
 mkdir -p zips
 rm zips/sony-dualsim-patcher-v$VERSION.zip
 zip zips/sony-dualsim-patcher-v$VERSION.zip tmp/* META-INF/com/google/android/update-binary META-INF/com/google/android/updater-script
